bcc53d947ebb6300e1de07048fba2b8832f44b9a
Vic Shóstak <vic@shostak.dev>
2021-09-11 08:12:14 +0300

    Add Cloudflare recommended server timeouts

--- pkg/configs/fiber_config.go
+++ pkg/configs/fiber_config.go
@@ -11,13 +11,25 @@ import (
 // FiberConfig func for configuration Fiber app.
 // See: https://docs.gofiber.io/api/fiber#config
 func FiberConfig() fiber.Config {
-	// Define server settings.
+	// Define server read timeout.
 	readTimeoutSecondsCount, err := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT"))
 	if err != nil {
 		return fiber.Config{}
 	}
 
-	// Define app settings for production.
+	// Define server write timeout.
+	writeTimeoutSecondsCount, err := strconv.Atoi(os.Getenv("SERVER_WRITE_TIMEOUT"))
+	if err != nil {
+		return fiber.Config{}
+	}
+
+	// Define server idle timeout.
+	idleTimeoutSecondsCount, err := strconv.Atoi(os.Getenv("SERVER_IDLE_TIMEOUT"))
+	if err != nil {
+		return fiber.Config{}
+	}
+
+	// Define server settings for production.
 	var startupMessage bool
 	if os.Getenv("STAGE_STATUS") == "prod" {
 		startupMessage = true
@@ -26,6 +38,8 @@ func FiberConfig() fiber.Config {
 	// Return Fiber configuration.
 	return fiber.Config{
 		DisableStartupMessage: startupMessage,
-		ReadTimeout:           time.Second * time.Duration(readTimeoutSecondsCount),
+		ReadTimeout:           time.Duration(readTimeoutSecondsCount) * time.Second,
+		WriteTimeout:          time.Duration(writeTimeoutSecondsCount) * time.Second,
+		IdleTimeout:           time.Duration(idleTimeoutSecondsCount) * time.Second,
 	}
 }
