9acb40761b718287fda8f4703ea48600431d25b0
Vic Shóstak <vic@shostak.dev>
2021-09-17 15:22:02 +0300

    Add more validation to Activation & Reset codes

--- app/models/activation_code_model.go
+++ app/models/activation_code_model.go
@@ -8,12 +8,12 @@ import (
 
 // ActivationCode struct to describe activation code object.
 type ActivationCode struct {
-	Code     string    `db:"code" json:"code" validate:"required"`
+	Code     string    `db:"code" json:"code" validate:"required,lte=14"`
 	ExpireAt time.Time `db:"expire_at" json:"expire_at" validate:"required"`
 	UserID   uuid.UUID `db:"user_id" json:"user_id" validate:"required,uuid"`
 }
 
 // ActivateAccount struct to describe account activation object.
 type ActivateAccount struct {
-	Code string `json:"code" validate:"required"`
+	Code string `json:"code" validate:"required,lte=14"`
 }
--- app/models/reset_code_model.go
+++ app/models/reset_code_model.go
@@ -4,17 +4,17 @@ import "time"
 
 // ResetCode struct to describe reset codes object.
 type ResetCode struct {
-	Code     string    `db:"code" json:"code" validate:"required"`
+	Code     string    `db:"code" json:"code" validate:"required,lte=14"`
 	ExpireAt time.Time `db:"expire_at" json:"expire_at" validate:"required"`
-	Email    string    `json:"email" validate:"required,email"`
+	Email    string    `json:"email" validate:"required,email,lte=255"`
 }
 
 // ResetPassword struct to describe forgot password object.
 type ForgotPassword struct {
-	Email string `json:"email" validate:"required,email"`
+	Email string `json:"email" validate:"required,email,lte=255"`
 }
 
 // ResetPassword struct to describe reset password object.
 type ResetPassword struct {
-	Code string `json:"code" validate:"required"`
+	Code string `json:"code" validate:"required,lte=14"`
 }
