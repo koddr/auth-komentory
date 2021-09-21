f7cd4329394266d11bcd2d005ac6619f41551e13
Vic Shóstak <vic@shostak.dev>
2021-09-21 08:36:04 +0300

    Add helmet; Add Fiber compress middleware

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/Komentory/utilities v0.5.1
 	github.com/gofiber/fiber/v2 v2.18.0
+	github.com/gofiber/helmet/v2 v2.2.1
 	github.com/gofiber/jwt/v2 v2.2.7
 	github.com/golang-jwt/jwt/v4 v4.0.0
 	github.com/google/uuid v1.3.0
@@ -34,7 +35,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.30.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
+	golang.org/x/crypto v0.0.0-20210920023735-84f357641f63 // indirect
 	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- pkg/middleware/fiber_middleware.go
+++ pkg/middleware/fiber_middleware.go
@@ -4,10 +4,12 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
 	"github.com/gofiber/fiber/v2/middleware/favicon"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/helmet/v2"
 )
 
 // FiberMiddleware provide Fiber's built-in middlewares.
@@ -15,10 +17,16 @@ import (
 func FiberMiddleware(a *fiber.App) {
 	// Add middlewares.
 	a.Use(
+		// Add helmet.
+		helmet.New(),
 		// Add CORS to each route.
 		cors.New(cors.Config{
-			AllowOrigins:     os.Getenv("ALLOW_ORIGINS"),
+			AllowOrigins:     os.Getenv("CORS_ALLOW_ORIGINS"),
 			AllowCredentials: true,
+		}),
+		// Add compression.
+		compress.New(compress.Config{
+			Level: compress.LevelBestSpeed, // 1
 		}),
 		// Add encrypt cookies.
 		encryptcookie.New(encryptcookie.Config{
